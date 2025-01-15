53e4c3d684e90936e227ee3707c2324be5f3b18a
vsvdev007 <156921282+vsvdev007@users.noreply.github.com>
2025-01-15 11:44:27 +0200

    picture

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,12 +13,14 @@ export class HomeComponent {
   constructor(private auth:AuthService) { }
 
   isAuthent = false;
+  email:string = '';
 
   groups:string[] =[];
 
   ngOnInit() {
     this.groups = this.auth.groups;
     this.isAuthent = this.auth.isAuthent;
+    this.email = this.auth.email;
   }
 
 
